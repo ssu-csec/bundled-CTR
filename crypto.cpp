b0ba395f424f4ef5e00d8f9b760d87be081e0a00
chancebro <103893729+chancebro@users.noreply.github.com>
2023-09-11 16:20:25 +0900

    Update crypto.cpp

--- crypto.cpp
+++ crypto.cpp
@@ -253,7 +253,41 @@ int search_counter_block(vector<int> bundle_list, int index)
 
 	return counter_index;
 }
+int search_next_counter_block(vector<byte> metadata, int index)//입력인덱스가 존재하는 번들 뒷 번들의 카운터가 위치한 index를 알아냄
+{
+   int i = index;
+   while(true)
+   {
+      if (metadata[i] == 0x00)
+      {   
+         return i;//뒤에 위치한 카운터의 인덱스 리턴
+         
+      }
+      i++;//인덱스 값을 상승시킨 ==>오른쪽으로 이동
+   }
 
+   return 0;
+}
+
+int search_index_counter(vector<byte> metadata, int index,int counter)//번들의 
+{
+   int index_sum = 0;
+   int index_counter = counter;
+   int start_index = search_counter_block(metadata,index);
+   int end_index = search_real_index(metadata,index);
+   int t = 0;
+   while (1) {
+      index_sum = +metadata[t];
+      if ((index_sum > start_index) && (index_sum < end_index)) {
+         index_counter++;
+      }
+      if (index_sum > end_index) {
+         break;
+      }
+      t++;
+   }
+   return index_counter;
+}
 class bundled_CTR
 {
 
@@ -274,77 +308,157 @@ public:
 
 	~bundled_CTR() {}
 
-	void Insertion(string text, int index)
-	{// 1) Modi_info 할당
-	Modi_info modi_info_instance;
+	Modi_info bundled_CTR::Insertion(string text, int index)
+{
+    Modi_info modi_info_instance;
 
-	// 2) bundle_list 생성
-	vector<byte> meta_plain = metadata_dec(this->meta_data, this->key, this->nonce);
-	vector<int> bundle_list = bundle_list_gen(meta_plain);
+    ECB_Mode<AES>::Encryption e;
+    ECB_Mode<AES>::Decryption d;
+    e.SetKey(this->key, sizeof(this->key)); 
+    d.SetKey(this->key, sizeof(this->key));
 
-	// 3) data가 삽입될 실제 인덱스와 번들 탐색
-	int real_index = search_real_index(meta_plain, index);
-	int head_ctr_add = search_counter_block(bundle_list, real_index);
+    // 2) bundle_list 생성
+    vector<byte> meta_plain = metadata_dec(this->meta_data, this->key, this->nonce);
+    vector<int> bundle_list = bundle_list_gen(meta_plain);
 
-	// 4) 메타 데이터의 첫 8바이트에 담긴 마지막 사용 카운터(C1) 습득
-	// Assuming meta_data stores the last used counter in the first 8 bytes
-	byte C1[AES::BLOCKSIZE/2];
-	memcpy(C1, meta_data.data(), AES::BLOCKSIZE/2);
+    // 3) data가 삽입될 실제 인덱스와 번들 탐색
+    int real_index = search_real_index(meta_plain, index);
+    int head_ctr_index = search_counter_block(meta_plain, real_index);
+    int next_counter_index = search_next_counter_block(meta_plain, real_index);
+    vector<byte> temp_bundle;
+    vector<byte> temp_metadata;
 
-	if (head_ctr_add != -1) // 삽입 index가 번들 중간에 위치하는가?
-	{
-		// yes) - 1) 삽입될 위치의 뒷 번들의 첫블록을 복호화하고 연속성 위한 카운터(C2) 습득
-		byte C2[AES::BLOCKSIZE/2];
-		// decrypt the following bundle's first block to get the C2
-		// ... (this part is not clear, you might need more implementation details)
+    // 4) 메타 데이터의 첫 8바이트에 담긴 마지막 사용 카운터(C1) 습득
+    byte C1[AES::BLOCKSIZE / 2];
+    memcpy(C1, this->meta_data.data(), AES::BLOCKSIZE / 2);
 
-		// yes) - 2) 마지막 카운터(C1)와 습득된 카운터(C2)를 이용하여 삽입 데이터 번들화
-		// ... (you will use C1 and C2 to create the bundled data for insertion)
+    if (real_index == head_ctr_index) //번들과 번들 사이에 삽입되는 경우
+    {
+        byte C2[AES::BLOCKSIZE / 2];
+        d.ProcessData(C2, (const byte*)(this->main_data.data() + next_counter_index), AES::BLOCKSIZE/2);
+        temp_bundle = encryption(this->nonce, C1, text, this->key, C2);
 
-	}
-	else
-	{
-		// no) - 1) 삽입 인덱스 기준으로 뒷부분을 암호화하는데 사용된 카운터 추정(C3)
-		byte C3[AES::BLOCKSIZE/2];
-		// ... (estimate the counter C3 based on the insertion index)
+        // 5) 삽입 번들 기존 번들사이에 삽입
+        main_data.insert(main_data.begin() + real_index, temp_bundle.begin(), temp_bundle.end());
 
-		// no) - 2) 추정된 카운터(C3)를 이용하여 번들 분화(앞 번들과 뒷번들로 분화)
-		// ... (you will use C3 to split the bundle into front and back parts)
+        int save = 0;
+        int sum = 0;
+        for(int i = 0; i < meta_plain.size(); i++)//삽입할 메타데이터의 인덱스를 알아내는 과정
+        {
+            sum += meta_plain[i];
+            if (meta_plain[i] == 0)
+            {
+                save = i;
+            }
+            if (sum > real_index)
+            {
+                break;
+            }
+        }
+        temp_metadata=metadata_gen(strnlen(text));
+        e.ProcessData()//메타데이터 암호화
+        // 6) 메타데이터에 메인데이터의 삽입사항 반영하여 업데이트
+        meta_plain.insert(meta_plain.begin() + save, temp_metadata.begin(), temp_metadata.end());
+    }
+    else
+    {
+        // no) - 1) 삽입 인덱스 기준으로 뒷부분을 암호화하는데 사용된 카운터 추정(C3)
+        int insert_block_index = search_block_index(meta_plain, index);
+        int insert_block_ctr = search_counter_block(meta_plain, insert_block_index);
 
-		// no) - 3) 마지막 카운터(C1)와 추정된 카운터(C3)를 사용하여 삽입 데이터 번들화
-		// ... (you will use C1 and C3 to create the bundled data for insertion)
-	}
+        byte C3[AES::BLOCKSIZE / 2];//추정카운터(latter_bundle이 암호화 되는데에 사용됬을것으로 보이는 카운터)
+        byte C4[AES::BLOCKSIZE / 2];//삽입할 인덱스가 포함된 번들의 카운터
+        byte C5[AES::BLOCKSIZE / 2];//삽입할 인덱스가 포함된 번들의 뒷번들의 카운터
+        d.ProcessData(C4, (const byte*)(this->main_data.data() + head_ctr_index), AES::BLOCKSIZE/2);//해당번들의 첫블록에서 C4를 가져옴
 
-	// 5) 삽입 번들 기존 번들사이에 삽입
-	// ... (you will insert the new bundled data in the middle of the existing data)
+        vector<byte> previous_bundle;//분화된 앞쪽번들
+        vector<byte> latter_bundle;//분화된 뒷쪽번들
 
-	// 6) 메타데이터에 메인데이터의 삽입사항 반영하여 업데이트
-	// ... (you will update the meta_data to reflect the changes in the main_data)}
+        int save = 0;
+        int sum = 0;
+        for(int i = 0; i < meta_plain.size(); i++)//뒷 블록의 카운터가 포함된 인덱스를 가져오는 과정
+        {
+            sum += meta_plain[i];//암호문에서의 위치
+            if (meta_plain[i] == 0)
+            {
+                save = i;//삽입될 인덱스가 포함될 다음 bundle의  metadata의위치
+            }
+            if ((sum > real_index) && (meta_plain[i] == 0))
+            {
+                break;
+            }
+        }
+        d.ProcessData(C5, (const byte*)(this->main_data.data() + sum), AES::BLOCKSIZE/2);//뒷블록 가져오는 중
 
-	Modi_info Deletion(int del_len, int index)
-	{
-		Modi_info modi_info();
-		vector<byte> meta_plain = metadata_dec(this->metadata, this->key, this->nonce);
-		vector<int> bundle_list = bundle_list_gen(meta_plain);
-		
-		int head = search_real_index(meta_plain, index);
-		int tail = search_real_index(meta_plain, index + del_len);
-		int head_ctr_add = search_counter_block(bundle_list, head);
-		int tail_ctr_add = search_counter_block(bundle_list, tail);
+        // no) - 2) 추정된 카운터(C3)를 이용하여 번들 분화(앞 번들과 뒷번들로 분화)
+        previous_bundle.assign(this->main_data.begin() + insert_block_ctr, this->main_data.begin() + insert_block_index);
+        latter_bundle.assign(this->main_data.begin() + insert_block_index, this->main_data.begin() + next_counter_index);
+        int C3_int = search_index_counter(meta_plain, real_index, (int)C4);
+        memcpy(C3, &C3_int, sizeof(C3_int));
 
-		ECB_Mode<AES>::Encryption e;
-		ECB_Mode<AES>::Decryption d;
-		e.SetKey(this->key, sizeof(this->key));
-		d.SetKey(this->key, sizeof(this->key));
-		byte head_counter[AES::BLOCKSIZE] = {0x00, };
-		byte tail_counter[AES::BLOCKSIZE] = {0x00, };
+        byte Counter[AES::BLOCKSIZE];
+        memcpy(Counter, C3, 8);
+        memcpy(Counter + 8, C5, 8);
+        latter_bundle.insert(latter_bundle.begin(), Counter, Counter + 16);//뒷번들에 첫블록 삽입
+        
+        memcpy(&previous_bundle[8], C1, 8);
 
-		this->main_data.erase(head, tail);
+        // no) - 3) 마지막 카운터(C1)와 추정된 카운터(C3)를 사용하여 삽입 데이터 번들화
+        temp_bundle = encryption(this->nonce, C1, text, this->key, C3);
+        main_data.insert(main_data.begin() + real_index, temp_bundle.begin(), temp_bundle.end());
+        int sum1=0;
+        int sum2=0;
+        int sum3=0;
+        int save1=0;
+        int save2=0;
+        int t=0;
+        int latter=0;
+        //메타데이터를 써야함
+        for(int i = 0; i < meta_plain.size(); i++)//뒷 블록의 카운터가 포함된 인덱스를 가져오는 과정
+        {   
+            sum1 += meta_plain[i];//암호문에서의 위치
+            sum2 += meta_plain[i];
+            if(t=!i){
+                sum3 += meta_plain[i];
+            }
+            if (meta_plain[i] == 0)
+            {
+                save1 = i;//삽입될 인덱스가 포함될 다음 bundle의  metadata의위치
+            }
 
-		// modify metadata and update
+            if ((sum > real_index))
+            {   
+                save2=i;
+                break;
+            }
+            t++;
+        }
+        //번들 분화할때 인덱스가포함된 번들값-바로 전 metadata 합값
+        byte need=sum3-sum1;
+        byte need2=meta_plain[save2]-need;
+        meta_plain[save2]=need;
+        //그걸 해당 인덱스 값으로 바꾸고 그 뒷값부터 번들 끝까지 날려
+        for(int i=save2+1;i<save;i++){
+            latter += meta_plain[i];        }
+        meta_plain.erase(meta_plain.begin() + save1, meta_plain.begin() + save);//save1부터 save까지 날려
+        //삽입 번들은 그냥 메타젠으로 만들고
+        vector<byte> new_matadata;
+        vector<byte> latter_matadata;
+        new_matadata=metadata_gen(strnlen(text));
+        //번들의 첫번째 블록
+        latter_matadata=metadata_gen(latter);
+        //1번인덱스에다가 need2 삽입
+        latter_matadata.insert(latter_matadata.begin()+1,need2);
+        //이거를 인설트
 
-		return modi_info;
-	}
+        //이값을 meta 젠 len한담에 
+
+    }
+
+   
+
+    
+}
 
 	void Replacement(string text, int index)
 	{}
